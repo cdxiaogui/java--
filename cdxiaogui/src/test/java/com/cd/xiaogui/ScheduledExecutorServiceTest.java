60d5a741dbe476b3c6d4ef6b67e3453d5c80aeb9
sunyawei3 <sunyawei3@jd.com>
2022-07-25 14:06:39 +0800

    延迟执行

---
+++ cdxiaogui/src/test/java/com/cd/xiaogui/ScheduledExecutorServiceTest.java
@@ -0,0 +1,33 @@
+package com.cd.xiaogui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 延迟处理
+ *
+ * @author sunyawei3
+ * 创建时间 2022/7/25 1:59 下午
+ */
+public class ScheduledExecutorServiceTest {
+
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
+        SimpleDateFormat sf = new SimpleDateFormat("HH:mm:ss");
+        scheduledExecutorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println(sf.format(new Date()));
+            }
+        }, 2, TimeUnit.SECONDS);
+        System.out.println("end");
+    }
+
+}
